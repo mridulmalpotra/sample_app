07c161aa9e24f5513e1c55a6fbb54eb9c47c363b
mridulmalpotra <mridul.malpotra@gmail.com>
2014-04-10 12:42:11 +0530

    Basic Google Login and CSS Templates

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,18 @@ SampleApp::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root :to => redirect('static_pages/home')
 
+GoogleAuthExample::Application.routes.draw do
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions#destroy', as: 'signout'
+ 
+  resources :sessions, only: [:create, :destroy]
+  resource :home, only: [:show]
+ 
+  root to: "home#show"
+end
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,11 +1,25 @@
 require 'spec_helper'
 
-describe "StaticPages" do
+
+describe "Static pages" do
+  
+  let (:base_title) {"Ruby on Rails Tutorial Sample App"}
+
   describe "Home page" do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
+    it "should have the title 'Sample App'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("#{base_title}")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
+    end
+
   end
 
   describe "Help page" do
@@ -13,13 +27,20 @@ describe "StaticPages" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("#{base_title} | Help")
+  	end
   end
 
   describe "About page" do
-    it "should have the content 'About us'" do
+    it "should have the content 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_content('About us')
+      expect(page).to have_content('About Us')
+    end
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("#{base_title} | About Us")
     end
   end
-
 end
