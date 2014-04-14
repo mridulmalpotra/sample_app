453e93a357e3d7e9d8f03ea1f260ab218244eb38
mridulmalpotra <mridul.malpotra@gmail.com>
2014-04-14 10:10:54 +0530

    Made User Pictures Database changes

--- app/models/user_pic.rb
+++ app/models/user_pic.rb
@@ -1,2 +1,5 @@
 class UserPic < ActiveRecord::Base
+	userpic.oauth_token = user.uid
+	validates :pic_name, presence: true
+	validates :path, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140410070432) do
+ActiveRecord::Schema.define(version: 20140410110833) do
+
+  create_table "user_pics", force: true do |t|
+    t.string   "oauth_token"
+    t.string   "pic_name"
+    t.string   "path"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "provider"
